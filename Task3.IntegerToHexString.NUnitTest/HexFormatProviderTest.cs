223ffd7d2776eeff464f00764d51cbddabda7090
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-17 21:43:27 +0300

    Complete Task3 "Integer number to string in hexademical format"

---
+++ Task3.IntegerToHexString.NUnitTest/HexFormatProviderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task3.IntegerToHexString.NUnitTest {
+
+    [TestFixture]
+    public class HexFormatProviderTest {
+
+        [TestCase(145, Result = "0x91")]
+        [TestCase(-145, Result = "-0x91")]
+        [TestCase(0, Result = "0x0")]
+        [TestCase(41837, Result = "0xA36D")]
+        public string FormatTest(int number) {
+            IFormatProvider fp = new HexFormatProvider();
+            return string.Format(fp, "{0:H}", number);
+        }
+    }
+}
---
+++ Task3.IntegerToHexString/HexFormatProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Task3.IntegerToHexString {
+    public class HexFormatProvider : IFormatProvider, ICustomFormatter {
+
+        private readonly IFormatProvider m_ParentProvider;
+
+        private static readonly string[] m_HexNumber = "0 1 2 3 4 5 6 7 8 9 A B C D E F".Split();
+
+        #region Constructors
+        public HexFormatProvider() : this(CultureInfo.CurrentCulture) { }
+        public HexFormatProvider(IFormatProvider parent) {
+            m_ParentProvider = parent;
+        }
+        #endregion
+
+        #region Public Methods
+        public object GetFormat(Type formatType) => formatType == typeof (ICustomFormatter) ? this : null;
+        
+
+        public string Format(string format, object arg, IFormatProvider formatProvider) {
+            if (arg == null || format != "H")
+                return string.Format(m_ParentProvider, "{0:" + format + "}", arg);
+
+            StringBuilder result = new StringBuilder();
+
+            if (!(arg is int) && !(arg is long)) 
+                throw new ArgumentException("Wrong argument type. Argument name: " + nameof(arg));    
+
+            long number = Convert.ToInt64(arg);
+            bool needMinus = false;
+            if (number < 0) {
+                number *= -1;
+                needMinus = true;
+            }
+
+            do {
+                int ch = (int) (number%16);
+                result.Append(m_HexNumber[ch]);
+                number /= 16;
+            } while (number > 0);
+
+            return (needMinus ? "-" : "") + "0x" + new string(result.ToString().Reverse().ToArray());
+        }
+        #endregion
+    }
+}
