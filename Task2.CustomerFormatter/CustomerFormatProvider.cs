1cb06f77781c8abc258bd38e86e3ecc2955bf7a9
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-19 17:24:37 +0300

    Refactor Task2

--- Task2.CustomerFormatter/CustomerFormatProvider.cs
+++ Task2.CustomerFormatter/CustomerFormatProvider.cs
@@ -26,48 +26,20 @@ namespace Task2.CustomerFormatter {
             Customer customer = arg as Customer;
             if (customer == null)
                 throw new ArgumentException("Wrong argument type. Argument name: " + nameof(arg));
-            
-            Regex reg = new Regex("[NRC\\s\\W]{1}");
-            if (!reg.IsMatch(format))
-                throw new ArgumentException("Unsupported format");
-            
-            StringBuilder result = new StringBuilder(format);
-            int nIndex = format.IndexOf("N", StringComparison.Ordinal);
-            int rIndex = format.IndexOf("R", StringComparison.Ordinal);
-            int cIndex = format.IndexOf("C", StringComparison.Ordinal);
 
-            int[] indexArrays = { nIndex, rIndex, cIndex};
-            char[] letterArray = {'N', 'R', 'C'};
-
-            Dictionary<char, string> dictionary = new Dictionary<char,string> {
-                {'N', customer.Name },
-                { 'R', customer.Revenue.ToString(CultureInfo.InvariantCulture) },
-                { 'C', customer.ContactPhone }
-            };
-            
-            for(int i = 3; i > 0; i--)
-                for(int j = 0; j < i - 1; j++)
-                    if (indexArrays[j] > indexArrays[j + 1]) {
-                        int tempIndex = indexArrays[j];
-                        char tempLetter = letterArray[j];
-                        indexArrays[j] = indexArrays[j + 1];
-                        letterArray[j] = letterArray[j + 1];
-                        indexArrays[j + 1] = tempIndex;
-                        letterArray[j + 1] = tempLetter;
-                    }
-
-            int insertIndex = 0;
-            for (int i = 0; i < 3; i++) {
-                if (indexArrays[i] >= 0) {
-                    insertIndex += indexArrays[i];
-                    if (i > 0 && indexArrays[i - 1] > 0)
-                        insertIndex -= indexArrays[i - 1];
-                    string insertString = dictionary[letterArray[i]];
-                    result = result.Remove(insertIndex, 1).Insert(insertIndex, insertString);
-                    insertIndex += insertString.Length - 1;
-                }
+            try {
+                string result = customer.ToString(format, formatProvider);
+                return result;
+            } catch (FormatException) {
+                
             }
-            return result.ToString();
+
+            switch (format) {
+                case "E":
+                    return $"Customer record: {customer.Name}\n\tRevenue: {customer.Revenue}\n\tContact phone: {customer.ContactPhone}";
+                default:
+                    throw new FormatException($"The {format} format string not supported");
+            }
         }
 
         #endregion
--- Task2.CustomerFormatterTest/CustomerFormatProviderTest.cs
+++ Task2.CustomerFormatterTest/CustomerFormatProviderTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using NUnit.Framework;
 using Task2.CustomerFormatter;
 
@@ -8,19 +9,16 @@ namespace Task2.CustomerFormatterTest {
     public class CustomerFormatProviderTest {
 
         private static readonly Customer m_Customer = new Customer();
-
+        //Customer record: Jeffrey Richter, 1000000.00, +1 (425) 555-0100"
         public IEnumerable<TestCaseData> TestDatas {
             get {
-                yield return new TestCaseData(m_Customer, "Customer record: {0:N, R, C}").Returns($"Customer record: {m_Customer.Name}, {m_Customer.Revenue}, {m_Customer.ContactPhone}");
-                yield return new TestCaseData(m_Customer, "Customer record: {0:C}").Returns($"Customer record: {m_Customer.ContactPhone}");
-                yield return new TestCaseData(m_Customer, "Customer record: {0:N, R}").Returns($"Customer record: {m_Customer.Name}, {m_Customer.Revenue}");
-                yield return new TestCaseData(m_Customer, "Customer record: {0:N}").Returns($"Customer record: {m_Customer.Name}");
-                yield return new TestCaseData(m_Customer, "Customer record: {0:R}").Returns($"Customer record: {m_Customer.Revenue}");
-                yield return new TestCaseData(m_Customer, "{0:N, C}").Returns($"{m_Customer.Name}, {m_Customer.ContactPhone}");
-                yield return new TestCaseData(m_Customer, "Customer record:\nName: {0:N}\nRevenue: {0:R}\nContactPhone: {0:C}").Returns($"Customer record:\nName: {m_Customer.Name}\nRevenue: {m_Customer.Revenue}\nContactPhone: {m_Customer.ContactPhone}");
+                yield return new TestCaseData(m_Customer, "{0:A}").Returns(m_Customer.ToString("A", CultureInfo.InvariantCulture));
 
-                yield return new TestCaseData(null, "Customer record: {0:N, R, C}").Throws(typeof(ArgumentException));
-                yield return new TestCaseData(m_Customer, "Customer record: {0:B}").Throws(typeof(ArgumentException));
+                yield return new TestCaseData(m_Customer, "{0:E}").Returns($"Customer record: {m_Customer.Name}\n\tRevenue: {m_Customer.Revenue}\n\tContact phone: {m_Customer.ContactPhone}");
+
+                yield return new TestCaseData(m_Customer, "{0:W}").Throws(typeof(FormatException));
+
+                yield return new TestCaseData(m_Customer, "{0:C}").Returns(m_Customer.ToString("C", CultureInfo.InvariantCulture));
             }
         } 
         
