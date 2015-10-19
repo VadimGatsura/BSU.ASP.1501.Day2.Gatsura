cac45e7d331f2b1976bbe5328858815c039828c1
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-19 15:08:53 +0300

    Additional test for Task3.

--- Task3.IntegerToHexString.NUnitTest/HexFormatProviderTest.cs
+++ Task3.IntegerToHexString.NUnitTest/HexFormatProviderTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Task3.IntegerToHexString.NUnitTest {
@@ -11,13 +13,21 @@ namespace Task3.IntegerToHexString.NUnitTest {
         [TestCase(0, Result = "0x0")]
         [TestCase(41837, Result = "0xA36D")]
         [TestCase(47, Result = "0x2F")]
-        public string Format_Test(int number) {
+        [TestCase(47.2, ExpectedException = typeof(ArgumentException))]
+        public string Format_Test(object number) {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             IFormatProvider fp = new HexFormatProvider();
             return string.Format(fp, "{0:H}", number);
         }
 
         [TestCase(47, "{0:X}", Result = "2F")]
-        public string ParentFormat_Test(int number, string format) {
+        [TestCase(.473, "{0:P}", Result = "47.30 %")]
+        [TestCase(.473, "{0:P0}", Result = "47 %")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.7321, "{0:F2}", Result = "4.73")]
+        public string ParentFormat_Test(object number, string format) {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             IFormatProvider fp = new HexFormatProvider();
             return string.Format(fp, format, number);
         }
