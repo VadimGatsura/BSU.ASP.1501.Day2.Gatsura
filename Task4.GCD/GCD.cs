55a8f022b3675c92a53b631c436270c3ae9b6880
Vadim Gatsura <vadimgatsura@gmail.com>
2015-11-01 17:07:14 +0300

    Refactor GCD

--- Task4.GCD/GCD.cs
+++ Task4.GCD/GCD.cs
@@ -5,6 +5,8 @@ using System.Linq;
 namespace Task4.GCD {
     public static class GCD {
 
+        private delegate long GCDAlgorithm(long a, long b);
+
         #region Public Methods
 
         #region Euclidean
@@ -14,18 +16,7 @@ namespace Task4.GCD {
         /// <param name="a">The first number</param>
         /// <param name="b">The second number</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Euclidean(long a, long b) {
-            if (a < 0)
-                a *= -1;
-            if (b < 0)
-                b *= -1;
-            while (b != 0) {
-                long temp = b;
-                b = a % b;
-                a = temp;
-            }
-            return a;
-        }
+        public static long Euclidean(long a, long b) => Calculate(a, b, EuclideanCalculate);
 
         /// <summary>
         /// Calculate the greatest common divisor by Euclidean algorithm
@@ -34,26 +25,14 @@ namespace Task4.GCD {
         /// <param name="b">The second number</param>
         /// <param name="ticks">The number of timer ticks that have been spent on the calculation</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Euclidean(long a, long b, out long ticks) {
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-            long result = Euclidean(a, b);
-            timer.Stop();
-            ticks = timer.ElapsedTicks;
-            return result;
-        }
+        public static long Euclidean(long a, long b, out long ticks) => Calculate(a, b, out ticks, EuclideanCalculate);
 
         /// <summary>
         /// Calculate the greatest common divisor by Euclidean algorithm
         /// </summary>
         /// <param name="array">Array of parameters for calculating the greatest common divisor</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Euclidean(params long[] array) {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
-
-            return array.Aggregate<long, long>(0, Euclidean);
-        }
+        public static long Euclidean(params long[] array) => Calculate(EuclideanCalculate, array);
 
         /// <summary>
         /// Calculate the greatest common divisor by Euclidean algorithm
@@ -61,12 +40,7 @@ namespace Task4.GCD {
         /// <param name="ticks">The number of timer ticks that have been spent on the calculation</param>
         /// <param name="array">Array of parameters for calculating the greatest common divisor</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Euclidean(out long ticks, params long[] array) {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
-
-            return Find(Euclidean, out ticks, array);
-        }
+        public static long Euclidean(out long ticks, params long[] array) => Calculate(EuclideanCalculate, out ticks, array);
         #endregion
 
         #region Stein
@@ -76,33 +50,7 @@ namespace Task4.GCD {
         /// <param name="a">The first number</param>
         /// <param name="b">The second number</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Stein(long a, long b) {
-            if (a < 0)
-                a *= -1;
-            if (b < 0)
-                b *= -1; 
-            if (a == b)
-                return b;
-
-            if (a == 0)
-                return b;
-
-            if (b == 0)
-                return a;
-
-            if (a %2 == 0) { 
-                if ( b % 2 != 0)
-                    return Stein(a >> 1, b);
-                return Stein(a >> 1, b >> 1) << 1;
-            }
-            if (b % 2 == 0) 
-                return Stein(a, b >> 1);
-
-            if (a > b)
-                return Stein((a - b) >> 1, b);
-
-            return Stein((b - a) >> 1, a);
-        }
+        public static long Stein(long a, long b) => Calculate(a, b, SteinCalculate);
 
         /// <summary>
         /// Calculate the greatest common divisor by Stein algorithm
@@ -111,25 +59,14 @@ namespace Task4.GCD {
         /// <param name="b">The second number</param>
         /// <param name="ticks">The number of timer ticks that have been spent on the calculation</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Stein(long a, long b, out long ticks) {
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-            long result = Stein(a, b);
-            timer.Stop();
-            ticks = timer.ElapsedTicks;
-            return result;
-        }
+        public static long Stein(long a, long b, out long ticks) => Calculate(a, b, out ticks, SteinCalculate);
 
         /// <summary>
         /// Calculate the greatest common divisor by Stein algorithm
         /// </summary>
         /// <param name="array">Array of parameters for calculating the greatest common divisor</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Stein(params long[] array) {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
-            return array.Aggregate<long, long>(0, Stein);
-        }
+        public static long Stein(params long[] array) => Calculate(SteinCalculate, array);
 
         /// <summary>
         /// Calculate the greatest common divisor by Stein algorithm
@@ -137,30 +74,82 @@ namespace Task4.GCD {
         /// <param name="ticks">The number of timer ticks that have been spent on the calculation</param>
         /// <param name="array">Array of parameters for calculating the greatest common divisor</param>
         /// <returns>The greatest common divisor</returns>
-        public static long Stein(out long ticks, params long[] array) {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
-            return Find(Stein, out ticks, array);
-        }
+        public static long Stein(out long ticks, params long[] array) => Calculate(SteinCalculate, out ticks, array);
         #endregion
 
         #endregion
 
         #region Private Methods
-
-        private delegate long Algorithm(long a, long b, out long ticks);
-
-        private static long Find(Algorithm find, out long ticks, params long[] array) {
+        private static long Calculate(GCDAlgorithm algorithm, out long ticks, params long[] array) {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
             long result = 0;
             ticks = 0;
-            long t;
             foreach (long element in array) {
-                result = find(result, element, out t);
+                long t;
+                result = Calculate(result, element, out t, algorithm);
                 ticks += t;
             }
             return result;
         }
-        #endregion
 
+        private static long Calculate(long a, long b, GCDAlgorithm algorithm) {
+            if (a < 0)
+                a *= -1;
+            if (b < 0)
+                b *= -1;
+            if (a == b)
+                return b;
+            if (a == 0)
+                return b;
+
+            return b == 0 ? a : algorithm(a, b);
+        }
+
+        private static long Calculate(long a, long b, out long ticks, GCDAlgorithm algorithm) {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            long result = Calculate(a, b, algorithm);
+            timer.Stop();
+            ticks = timer.ElapsedTicks;
+            return result;
+        }
+
+        private static long Calculate(GCDAlgorithm algorithm, params long[] array) {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            return array.Aggregate<long, long>(0, (a, b) => Calculate(a, b, algorithm));
+        }
+
+        private static long EuclideanCalculate(long a, long b) {
+            while (b != 0) {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private static long SteinCalculate(long a, long b) {
+            int shift;
+            for (shift = 0; ((a | b) & 1) == 0; ++shift) {
+                a >>= 1;
+                b >>= 1;
+            }
+            while ((a & 1) == 0)
+                a >>= 1;
+            do {
+                while ((b & 1) == 0)
+                    b >>= 1;
+                if (a > b) {
+                    long t = b;
+                    b = a;
+                    a = t;
+                }
+                b = b - a;
+            } while (b != 0);
+            return a << shift;
+        }
+        #endregion
     }
 }
